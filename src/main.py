8b1ae77bc038088bcd0ff2e41a7419ac6a86fcc2
ayo107 <ayo_linkin10@hotmail.com>
2022-06-17 10:37:45 +0000

    primera subida

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -30,6 +30,55 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route("/task", methods=["POST"])
+def create_task():
+    body = request.get_json()
+
+    task = Task(text=body["text"], done=False)
+    db.session.add(task)
+    db.session.commit()
+    return jsonify(task.serialize())
+
+
+@app.route('/task', methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    all_tasks = list(map(lambda task: task.serialize(), tasks))
+
+    return jsonify(all_tasks)
+
+
+@app.route('/task/<int:task_id>', methods=["PUT", "GET", "DELETE"])
+def handle_task(task_id):
+    if request.method == "PUT":
+        task = Task.query.get(task_id)
+        if task is None:
+            raise APIException("Tarea No Encontrada", 404)
+        body = request.get_json()
+
+        if not ("done" in body):
+            raise APIException("parametro done no encontrado", 404)
+        task.done = body["done"]
+        db.session.commit()
+
+        return jsonify(task.serialize())
+
+    elif request.method == "GET":
+        task = Task.query.get(task_id)
+        if task is None:
+            raise APIException("Tarea No Encontrada", 404)
+
+        return jsonify(task.serialize())
+
+    elif request.method == "DELETE":
+        task = Task.query.get(task_id)
+        if task is None:
+            raise APIException("Tarea No Encontrada", 404)
+        db.session.delete(task)
+        db.session.commit()
+        return jsonify(task.serialize())
+
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -38,6 +87,7 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
